17a9fead896d5572fcad2e12c512ba18244b193f
HlogiB <bale.forthwith@gmail.com>
2023-06-12 16:01:55 +0200

    second attempt

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -5,7 +5,7 @@ import time as tm
 import string
 root=Tk()
 root.geometry("400x600")
-root.title("4KIDZ GIT ")
+root.title("4KIDZ GIT Conflict ")
 #This is the second file conflict
 engine = pyttsx3.init()
 engine.setProperty('rate',160)
